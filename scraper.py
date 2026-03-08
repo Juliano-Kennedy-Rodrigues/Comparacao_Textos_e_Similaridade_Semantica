24ceeb3ef1ed592c6c5602e60278a8156caf7a20
Juliano Kennedy Rodrigues <julianokennedy11@gmail.com>
2026-03-08 17:55:45 -0300

    arrumando o scraper para não pegar resumo varias vezes, comparacao basica feita

--- scraper.py
+++ scraper.py
@@ -5,11 +5,14 @@ import requests
 from bs4 import BeautifulSoup
 from langdetect import detect
 import time
+import os 
 
 #salvando oq peguei do scielo num json para poder fazer fine tuning depois
 def save_data(data, file_name="scielo.json"):
     try:
-        with open(file_name, "w", encoding="utf-8") as f: #abro arquivo pra write
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(base_dir, "scielo.json")
+        with open(file_path, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
         print('Salvou no json')
     except Exception as e:
@@ -18,7 +21,7 @@ def save_data(data, file_name="scielo.json"):
 
 def get_papers(num_pages = 1):
     #scielo filtrado pro Brasil
-    url = 'https://search.scielo.org/?fb=&where=&filter%5Bin%5D%5B%5D=scl'
+    url = 'https://search.scielo.org/?fb=&q=*&lang=pt&where=&filter%5Bin%5D%5B%5D=scl&filter%5Bla%5D%5B%5D=pt&filter%5Btype%5D%5B%5D=research-article'
     results = []
     
     headers = {
@@ -32,7 +35,7 @@ def get_papers(num_pages = 1):
         print(f"Nessa página {page}")
         #https://search.scielo.org/?fb=&where=&filter%5Bin%5D%5B%5D=scl&from=16&page=2
         params = {
-            'q': '*',
+            'q': '(ab:(PT))',
             'where': 'scl', #where=&filter%5Bin%5D%5B%5D=sc
             'from': (page-1)*15 + 1, #from é pq exibe 15 artigos por página
             'filter[in][]': 'scl'
@@ -40,59 +43,63 @@ def get_papers(num_pages = 1):
         
         #entra na pag e pega todo o html
         response = requests.get(url, params=params, headers=headers)
-        html = BeautifulSoup(response.text, 'html.parser')                
+        html = BeautifulSoup(response.text, 'html.parser')  
+        
+        for social in html.select('.socialLinks, .articleShareLink'):
+            social.decompose()      
+        
+        primeiro_link = html.select_one('.item .title') 
+        if primeiro_link:
+            print(f"O Python está vendo este primeiro: {primeiro_link.get_text()}")        
             
         ##S0101-28002026000200303-scl > div.col-md-11.col-sm-10.col-xs-11 > div:nth-child(1) > a (classe da div é 'line')
             
         #pega o título que tem o link
-        papers = html.select('.item .line a')
-        valid_papers = [p for p in papers if 'script=sci_arttext' in p.get('href', '')] #pega só os links que levam a um artigo, se tiver link de rede social, ou pesquisa, pula
         
-        for vp in valid_papers:
-            ptTitle = vp.get('title') #o título em PORTUGUÊS está dentro do <a> no title
+        #S0101-28002026000200301-scl > div.col-md-11.col-sm-10.col-xs-11
+        #S0101-28002026000100304-scl > div.col-md-11.col-sm-10.col-xs-11
+        
+        #papers = html.select_one('.content .searchForm .container resultBlock .col-md-9 col-sm-8 .results .item  .line a')
+        papers_list = html.select('.item .line a[href*="sci_arttext"]')
+        print('coletou algo sim')
+
+        for papers in papers_list:        
+            paper_title = papers.get('title')
+            paper_url = papers.get('href')
+
+            not_this = ["Compartilhar Facebook", " Twitter", "Português", "Inglês", "Espanhol", "Francês"]
+            if not paper_title or paper_title in not_this:
+                continue
             
-            #tento ver se tem título em pt, para garantir que não vai dar ruim no meu Bertimbau
-            if not ptTitle:
-                ptTitle = vp.get_text(strip=True)
-                
-            if len(ptTitle) < 20: 
-                continue
-
             try:
-                if detect(ptTitle) == "pt":
-                    paper_title = ptTitle
-                    paper_url = vp['href']
-
-                    #pegar resumo
-                    print(f"  -> Extraindo resumo de: {paper_title[:50]}...")
-                    abstract = get_abstract(paper_url, headers) 
-                
-                    if (abstract):
-                        results.append({"Título": paper_title, "resumo": abstract})
-                        print("COLETOU MSM")
-                        time.sleep(2)
-                        
-                    if abstract:
-                        item = {"Título": paper_title, "resumo": abstract}
-                        results.append(item) #pego o titulo = o titulo em portigues e o resumo que peguei juntando os <p> 
-                        save_data(results)
-                        time.sleep(2) #pra não levar um ban cis s2
-                                        
-                else:
-                    #se o título não for em pt, pula. Comentando pro terminal não ficar cheio
-                    #print(f"Pulando artigo em inglês: {ptTitle[:30]}...")
-                    pass
-            except:
+                if detect(paper_title) != 'pt':
+                    continue
+            except Exception as e: 
+                print(e)
                 continue
-        
+
+            print(f" -> Extraindo resumo de: {paper_title[:50]}...")
+            abstract = get_abstract(paper_url, headers)
+            
+            if abstract:
+                item = {"Título": paper_title, "resumo": abstract}
+                results.append(item)
+                save_data(results)
+                time.sleep(2)
+            else:
+                print(f"  -> AVISO: Resumo não encontrado para: {paper_title[:30]}")
 
 def get_abstract(paper_url, headers):  
     try:
         response = requests.get(paper_url, headers=headers)
         html = BeautifulSoup(response.text, 'html.parser')
+        
     
         #articleText > div.articleSection.articleSection--resumo --> achei inspecionando com F12
         abstract_div = html.select_one('div.articleSection.articleSection--resumo')
+        
+        if not abstract_div:
+            abstract_div = html.select_one('#resumo, #abstract')
         
         #se eu conseguir entrar na página e pegar a div com o RESUMO
         if abstract_div:
